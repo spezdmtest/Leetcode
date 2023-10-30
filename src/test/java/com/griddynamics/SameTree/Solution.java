ad03db9126041633ef0c1daae0cd76cd9dfe5622
spezdmtest <spezd.m.@gmail.com>
2023-10-30 14:58:15 +0200

    refactoring SameTree

--- src/test/java/com/griddynamics/SameTree/Solution.java
+++ src/test/java/com/griddynamics/SameTree/Solution.java
@@ -1,32 +1,33 @@
 package com.griddynamics.SameTree;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
  * Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: p = [1,2,3], q = [1,2,3]
  * Output: true
  * Example 2:
- *
- *
+ * <p>
+ * <p>
  * Input: p = [1,2], q = [1,null,2]
  * Output: false
  * Example 3:
- *
- *
+ * <p>
+ * <p>
  * Input: p = [1,2,1], q = [1,1,2]
  * Output: false
- *
- *
+ * <p>
+ * <p>
  * Constraints:
- *
+ * <p>
  * The number of nodes in both trees is in the range [0, 100].
  * -104 <= Node.val <= 104
  */
@@ -60,16 +61,16 @@ public class Solution {
 
 class TreeNodeTest {
     @Test
-   public void testIsSameTree() {
-       TreeNode p = new TreeNode(1);
-       p.left = new TreeNode(2);
-       p.right = new TreeNode(3);
+    public void testIsSameTree() {
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
 
-       TreeNode q = new TreeNode(1);
-       q.left = new TreeNode(2);
-       q.right = new TreeNode(3);
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
 
-       Solution solution = new Solution();
-       assertTrue(solution.isSameTree(p,q));
-   }
+        Solution solution = new Solution();
+        assertTrue(solution.isSameTree(p, q));
+    }
 }
