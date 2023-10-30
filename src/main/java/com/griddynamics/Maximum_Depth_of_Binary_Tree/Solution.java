e7aa9829cc9c551cafd46b6759a3c4cd84008dd8
spezdmtest <spezd.m.@gmail.com>
2023-10-30 15:19:17 +0200

    Maximum Depth of Binary Tree

--- src/main/java/com/griddynamics/Maximum_Depth_of_Binary_Tree/Solution.java
+++ src/main/java/com/griddynamics/Maximum_Depth_of_Binary_Tree/Solution.java
@@ -3,6 +3,11 @@ package com.griddynamics.Maximum_Depth_of_Binary_Tree;
 /**
  * Given the root of a binary tree, return its maximum depth.
  * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
  * Input: root = [3,9,20,null,null,15,7]
  * Output: 3
  * Example 2:
