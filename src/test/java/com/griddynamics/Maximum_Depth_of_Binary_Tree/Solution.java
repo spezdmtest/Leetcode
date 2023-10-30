a75f827cf23d51415bb75a3f73bd595b0dcae364
spezdmtest <spezd.m.@gmail.com>
2023-10-30 15:19:41 +0200

    Testing Maximum Depth of Binary Tree

---
+++ src/test/java/com/griddynamics/Maximum_Depth_of_Binary_Tree/Solution.java
@@ -0,0 +1,73 @@
+package com.griddynamics.Maximum_Depth_of_Binary_Tree;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Given the root of a binary tree, return its maximum depth.
+ * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ * Example 2:
+ * <p>
+ * Input: root = [1,null,2]
+ * Output: 2
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * The number of nodes in the tree is in the range [0, 104].
+ * -100 <= Node.val <= 100
+ *
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftMaxDepth = maxDepth(root.left);
+        int rightMaxDepth = maxDepth(root.right);
+//        return leftMaxDepth > rightMaxDepth ? leftMaxDepth + 1 : rightMaxDepth + 1;
+        return Math.max(leftMaxDepth, rightMaxDepth) + 1;
+    }
+}
+
+class TreeNodeTest {
+    @Test
+    public void testMaxDepth() {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        Solution solution = new Solution();
+        int depth = solution.maxDepth(root);
+
+        assertEquals(3, depth);
+    }
+
+}
