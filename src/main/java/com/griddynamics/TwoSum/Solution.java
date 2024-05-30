d51e6db0e9426944b9c3a4f90c5028004d31d74c
spezdmtest <spezd.m.@gmail.com>
2024-05-30 10:46:25 +0300

    AddTwoNumbers

--- src/main/java/com/griddynamics/TwoSum/Solution.java
+++ src/main/java/com/griddynamics/TwoSum/Solution.java
@@ -33,15 +33,8 @@ public class Solution {
     }
 
     public static int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-            int diff = target - nums[i];
-            if (map.containsKey(diff)) {
-                return new int[]{map.get(diff), i};
-            }
-        }
-        return new int[0];
+
+        return null;
     }
 
     // сумма элементов - значения
